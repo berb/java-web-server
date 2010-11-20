3927a3e7996be12b030190d82b84cc9fbacf5131
Benjamin Erb <mail@benjamin-erb.de>
2010-11-20 18:34:57 +0100

    added package structure

---
+++ src/de/ioexception/www/Http.java
@@ -0,0 +1,21 @@
+package de.ioexception.www;
+
+/**
+ * Class for general HTTP-related constants.
+ * 
+ * @author Benjamin Erb
+ *
+ */
+public final class Http
+{
+	public static final String CRLF = "\r\n";
+	public static final String HTTP = "HTTP";
+	public static final String PROTOCOL_DELIMITER = "/";
+	public static final String CONTENT_LENGTH = "Content-Length";
+
+	
+	private Http()
+	{
+		// no instances...
+	}
+}
